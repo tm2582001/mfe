e1b9beb81cac10c58f4594e4d9569551f57dcbef
Tushar Mehta <tm2582001@gmail.com>
2025-06-22 17:35:15 +0530

    workflow test

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,6 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common")
 const packageJson = require("../package.json");
 
+
 const devConfig = {
     mode: "development",
     devServer:{
