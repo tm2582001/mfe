5418d3a0834e6575b0818cf5f1b4882fd1dc5f6f
Tushar Mehta <tm2582001@gmail.com>
2025-06-26 21:55:01 +0530

    added header section and fixed images

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -114,12 +114,12 @@ export default function Album() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
+            {cards.map((card, i) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image={`https://picsum.photos/900?random=${i}`}
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
