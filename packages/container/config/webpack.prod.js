f773589972f3f0f20a09af50b11482c29eed6d6d
Tushar Mehta <tm2582001@gmail.com>
2025-07-02 15:04:48 +0530

    updated production config for container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,8 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const marketingDomain = process.env.MARKETING_PRODUCTION_DOMAIN;
+const authDomain = process.env.AUTH_PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: "production",
@@ -16,8 +17,8 @@ const prodConfig = {
             name: "container",
             remotes: {
                 // marketing: `marketing@${domain}/marketing/remoteEntry.js`
-                marketing: `marketing@${domain}/remoteEntry.js`,
-                auth: `auth@${domain}/remoteEntry.js`
+                marketing: `marketing@${marketingDomain}/remoteEntry.js`,
+                auth: `auth@${authDomain}/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
